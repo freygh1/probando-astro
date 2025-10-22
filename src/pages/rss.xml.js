864eadd8a0486532b2c5acc25803feb8cfceb719
freygh1 <freilinshernandez10@gmail.com>
2025-10-22 14:06:25 -0400

    change

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,10 +2,10 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    thetitle: 'Alumno de Astro | Blog',
+    title: 'Alumno de Astro | Blog',
     description: 'Mi viaje de aprendizaje de Astro',
-    site: context.site,
+    site: 'context.site',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>es-es</language>`,
+    customData: `<language>en-us</language>`,
   });
 }
